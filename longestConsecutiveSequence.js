0dded915b9f602e6ec350ef206ea53a68012fadb
Shubham Katwale <76983578+Shubhs1112@users.noreply.github.com>
2023-07-27 18:46:51 +0530

    Update longestConsecutiveSequence.js

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -32,3 +32,6 @@ var longestConsecutive = function(nums) {
     }
     return Math.max(...myArr);
 };
+
+console.log(longestConsecutive([100,4,200,1,3,2]));
+console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1]));
